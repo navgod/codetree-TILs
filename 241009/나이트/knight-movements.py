52e15e6f2ab2f78c4bb763950f2d52aec1f132c2
navgod <buzz2604@gmail.com>
2024-10-09 23:57:19 +0900

    Title: 나이트, Time: 223ms, Memory: 25MB, Status: Passed - CodeTree

--- 241009/나이트/knight-movements.py
+++ 241009/나이트/knight-movements.py
@@ -3,9 +3,8 @@ from collections import deque
 n = int(input())
 r1,c1,r2,c2 = map(int, input().split())
 
-step = [[0]*n for _ in range(n)]
+step = [[-1]*n for _ in range(n)]
 visited = [[0]*n for _ in range(n)]
-step[c2-1][r2-1] = -1
 q = deque()
 
 def can_go(x,y):
@@ -21,12 +20,14 @@ def bfs():
                 visited[ny][nx] = 1
                 step[ny][nx] = step[y][x] +1
                 q.append((nx,ny))
+                if nx == c2-1 and ny == r2-1:
+                    return
                 
 
 q.append((c1-1,r1-1))
-visited[c1-1][r1-1] = 1
-step[c1-1][r1-1] = 0
+visited[r1-1][c1-1] = 1
+step[r1-1][c1-1] = 0
 
 bfs()
 
-print(step[c2-1][r2-1])
+print(step[r2-1][c2-1])
