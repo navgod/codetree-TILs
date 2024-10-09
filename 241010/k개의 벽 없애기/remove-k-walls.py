26c6a7cebc75d88f2bbd24132356485f470a1d60
navgod <buzz2604@gmail.com>
2024-10-10 00:39:59 +0900

    Title: k개의 벽 없애기, Time: 155ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241010/k개의 벽 없애기/remove-k-walls.py
@@ -0,0 +1,71 @@
+from collections import deque
+n,k = map(int , input().split())
+
+q = deque()
+
+area = [list(map(int , input().split()))for _ in range(n)]
+visited = [[0]*n for _ in range(n)]
+step = [[-1]*n for _ in range(n)]
+walls = []
+selected_walls = []
+ans = -1
+
+for x in range(n):
+    for y in range(n):
+        if area[y][x] == 1:
+            walls.append((x,y))
+r1, c1 = map(int , input().split())
+r2, c2 = map(int , input().split())
+
+def can_go(x,y):
+    return 0<= x<n and 0<=y <n and not visited[y][x] and area[y][x] ==0
+
+def push(x,y,s):
+    visited[y][x] = 1
+    step[y][x] = s
+    q.append((x,y))
+
+def bfs():
+    dxs,dys = [1,-1,0,0] , [0,0,1,-1]
+
+    while q:
+        x, y = q.popleft()
+        for dx , dy in zip(dxs,dys):
+            nx , ny = x+dx , y +dy
+            if can_go(nx,ny):
+                push(nx,ny,step[y][x]+1)
+    
+def calc():
+    for i in range(n):
+        for j in range(n):
+            visited[j][i] = 0
+            step[j][i] = -1
+    
+    for x, y in selected_walls:
+        area[y][x] = 0
+    
+    x, y = c1-1 ,r1-1
+
+    push(x,y,0)
+
+    bfs()
+
+    for x, y in selected_walls:
+        area[y][x] = 1
+    
+    return step[r2-1][c2-1]
+
+
+def find_max(idx,cnt):
+    global ans
+    if cnt > k or idx == len(walls):
+        if cnt == k:
+            ans = max(ans, calc())
+        return
+    selected_walls.append(walls[idx])
+    find_max(idx+1,cnt+1)
+    selected_walls.pop()
+    find_max(idx+1,cnt)
+
+find_max(0,0)
+print(ans)
