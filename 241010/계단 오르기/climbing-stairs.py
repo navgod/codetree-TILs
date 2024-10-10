fb83f5119bcac81817ac26ec1f03376993d8d9e7
navgod <buzz2604@gmail.com>
2024-10-10 23:45:49 +0900

    Title: 계단 오르기, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 241010/계단 오르기/climbing-stairs.py
+++ 241010/계단 오르기/climbing-stairs.py
@@ -5,6 +5,8 @@ dp = [0] *(n+10)
 dp[2] = 1
 dp[3] = 1
 dp[4] = 1
-for i in range(5,n+1):
-    dp[i] = dp[i-2] + dp[i-3]
+dp[5] = 2
+dp[6] = 2
+for i in range(7,n+1):
+    dp[i] = (dp[i-2] + dp[i-3])%10007
 print(dp[n])
