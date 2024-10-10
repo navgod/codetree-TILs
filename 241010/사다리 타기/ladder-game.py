fbbbc5766e9d899e5c056d35eb6eb2c257418d92
navgod <buzz2604@gmail.com>
2024-10-10 21:41:36 +0900

    Title: 사다리 타기, Time: 5098ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

---
+++ 241010/사다리 타기/ladder-game.py
@@ -0,0 +1,37 @@
+import copy
+n, m = map(int , input().split())
+
+lines = [list(map(int , input().split())) for _ in range(m)]
+now = [i for i in range(1,n+1)]
+sorted_lines = sorted(lines, key = lambda x: x[1])
+
+def shift(a):
+    now[a-1] , now[a] = now[a] , now[a-1]
+
+for a, b in sorted_lines:
+    shift(a)
+
+target = copy.deepcopy(now)
+
+selected_line = []
+
+def check():
+    temp = [i for i in range(1,n+1)]
+    for a in selected_line:
+        temp[a-1], temp[a] = temp[a], temp[a-1]
+    return temp == target
+ans = m+1
+def min_cnt(cnt):
+    global ans
+    if check():
+        ans = min(ans ,cnt)
+        return 
+    if cnt >=m:
+        ans = min(ans ,m)
+        return 
+    for i in range(1,n):
+        selected_line.append(i)
+        min_cnt(cnt+1)
+        selected_line.pop()
+min_cnt(0)
+print(ans)
