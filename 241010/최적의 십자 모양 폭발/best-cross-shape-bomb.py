c4cd7e037e27a45fb88e8fb0cd1b57e61942f06c
navgod <buzz2604@gmail.com>
2024-10-10 16:03:37 +0900

    Title: 최적의 십자 모양 폭발, Time: 2962ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 241010/최적의 십자 모양 폭발/best-cross-shape-bomb.py
+++ 241010/최적의 십자 모양 폭발/best-cross-shape-bomb.py
@@ -9,15 +9,17 @@ grid = copy.deepcopy(grid_ori)
 new_grid = [[0]*n for _ in range(n)]
 
 def gravity():
+    global grid
     for x in range(n):
         new_y = n-1
         for y in range(n-1,-1,-1):
             if grid[y][x] != 0:
                 new_grid[new_y][x] = grid[y][x]
                 new_y -= 1
+    grid = copy.deepcopy(new_grid)
     for i in range(n):
         for j in range(n):
-            grid[i][j] , new_grid[i][j] = new_grid[i][j] , 0
+            new_grid[i][j] = 0
 
 def is_range(x,y):
     return 0<=x<n and 0<=y <n
