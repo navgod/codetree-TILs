db7b4191313e5af59de6afb749d009643390ec9a
navgod <buzz2604@gmail.com>
2024-10-10 23:16:06 +0900

    Title: 알파벳과 사칙연산, Time: 171ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 241010/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+target = input()
+
+selected = []
+
+ans = 0
+
+def calc():
+    val = selected[0]
+    for i in range(1,len(target),2):
+        if selected[i] == '*':
+            val *= selected[i+1]
+        elif selected[i] == '-':
+            val -= selected[i+1]
+        elif selected[i] == '+':
+            val += selected[i+1]
+    return val
+
+def simul(idx):
+    global ans
+
+    if idx == len(target):
+        ans = max(ans,calc())
+        return
+
+    if idx %2 ==0:
+        for i in range(1,5):
+            selected.append(i)
+            simul(idx+1)
+            selected.pop()
+    else:
+        selected.append(target[idx])
+        simul(idx+1)
+        selected.pop()
+
+simul(0)
+print(ans)
