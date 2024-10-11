bf042bd41fac3cc7a01a7bdfbb854e48b5d30bcf
navgod <buzz2604@gmail.com>
2024-10-11 12:49:49 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241011/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,25 @@
+n,m,k = map(int , input().split())
+k -=1
+grid = [list(map(int , input().split())) for _ in range(n)]
+
+now_row = -1
+
+def fall_down():
+    global now_row
+    stop = False
+    next_row = now_row +1
+    for i in range(k,k+m):
+        if grid[next_row][i] !=0:
+            stop = True
+            break
+    if not stop:
+        now_row = next_row
+        fall_down()
+
+fall_down()
+for i in range(k,k+m):
+    grid[now_row][i] = 1
+for row in grid:
+    for col in row:
+        print(col, end =' ')
+    print()
