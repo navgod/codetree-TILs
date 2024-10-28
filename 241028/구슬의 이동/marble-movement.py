41e3d4501543ec94c282ff4ff46d3080f9212db9
navgod <buzz2604@gmail.com>
2024-10-28 18:41:35 +0900

    Title: 구슬의 이동, Time: 266ms, Memory: 27MB, Status: Passed - CodeTree

--- 241028/구슬의 이동/marble-movement.py
+++ 241028/구슬의 이동/marble-movement.py
@@ -1,61 +1,93 @@
-dir_map = {
-    'U':0,
-    'D':2,
-    'R':1,
-    'L':3
-}
-dxs,dys = [0,1,0,-1] , [-1,0,1,0]
+# 변수 선언 및 입력:
+n, m, t, k = tuple(map(int, input().split()))
+grid = [
+    [[] for _ in range(n)]
+    for _ in range(n)
+]
+next_grid = [
+    [[] for _ in range(n)]
+    for _ in range(n)
+]
 
-def is_range(x,y):
-    return 0<=x<n and 0<=y<n
 
-def move(bead):
-    r,c,d,v = bead
-    x,y = c-1,r-1
-    dx,dy = dxs[d], dys[d]
-    for i in range(v):
-        if not is_range(x+dx,y+dy):
-            d = (d+2)%4
-            dx,dy = dxs[d], dys[d]
-        x,y = x+dx, y+dy
-    return [y+1,x+1,d,v]
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
 
-def check():
-    # 속도가 큰순위 같으면 idx가 큰순위
-    idx = 0
-    n_beads = []
-    for r,c,d,v in beads:
-        grid[r-1][c-1].append((v,idx))
-        idx+=1
+
+def next_pos(x, y, vnum, move_dir):
+    dxs, dys = [-1, 0, 0, 1], [0, 1, -1, 0]
+    
+    # vnum 횟수만큼 이동한 이후의 위치를 반환합니다.
+    for _ in range(vnum):
+        nx, ny = x + dxs[move_dir], y + dys[move_dir]
+        # 벽에 부딪히면
+        # 방향을 바꾼 뒤 이동합니다.
+        if not in_range(nx, ny):
+            move_dir = 3 - move_dir
+            nx, ny = x + dxs[move_dir], y + dys[move_dir]
+        x, y = nx, ny
+
+    return (x, y, move_dir)
+
+
+def move_all():
     for x in range(n):
         for y in range(n):
-            if len(grid[y][x]) >k:
-                target = grid[y][x]
-                selected_beads = sorted(target, key= lambda x: (x[0],x[1]), reverse = True)
-                for i in range(k):
-                    n_idx = selected_beads[i][1]
-                    n_beads.append(beads[n_idx])
-            else:
-                if grid[y][x]:
-                    for v,i in grid[y][x]:
-                        if i>= len(beads):
-                            print(i, len(beads), beads)
-                        else:
-                            n_beads.append(beads[i])
-            grid[y][x].clear()
-    return n_beads
+            for v, num, move_dir in grid[x][y]:
+                next_x, next_y, next_dir = next_pos(x, y, v, move_dir)
+                next_grid[next_x][next_y].append((v, num, next_dir))
 
 
-n,m,t,k = map(int, input().split())
-beads = [[int(x[0]), int(x[1]), dir_map[x[2]], int(x[3])] for x in (input().split() for _ in range(m))]
-grid = [[[] for _ in range(n)] for _ in range(n)]
+def select_marbles():
+    for i in range(n):
+        for j in range(n):
+            if len(next_grid[i][j]) >= k:
+                # 우선순위가 높은 k개만 남겨줍니다.
+                next_grid[i][j].sort(key=lambda x: (-x[0], -x[1]))
+                while len(next_grid[i][j]) > k:
+                    next_grid[i][j].pop()
+            
 
+def simulate():
+    # Step1. next_grid를 초기화합니다.
+    for i in range(n):
+        for j in range(n):
+            next_grid[i][j] = []
+		
+    # Step2. 구슬들을 전부 움직입니다.
+    move_all()
+    
+    # Step3. 각 칸마다 구슬이 최대 k개만 있도록 조정합니다.
+    select_marbles()
+    
+    # Step4. next_grid 값을 grid로 옮겨줍니다.
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = next_grid[i][j]
+
+
+dir_mapper = {
+    "U": 0,
+    "R": 1,
+    "L": 2,
+    "D": 3
+}
+
+for i in range(m):
+    r, c, d, v = tuple(input().split())
+    r, c, v = tuple(map(int, [r, c, v]))
+
+    # 살아남는 구슬의 우선순위가 더 빠른 속도, 더 큰 번호 이므로
+    # (속도, 방향, 번호) 순서를 유지합니다.
+    grid[r - 1][c - 1].append((v, i + 1, dir_mapper[d]))
+
+# t초에 걸쳐 시뮬레이션을 반복합니다.
 for _ in range(t):
-    new_beads = []
-    for bead in beads:
-        new_bead = move(bead)
-        new_beads.append(new_bead)
-    beads = new_beads
-    beads = check()
+    simulate()
 
-print(len(beads))
+ans = sum([
+    len(grid[i][j])
+    for i in range(n)
+    for j in range(n)
+])
+print(ans)
