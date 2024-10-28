44c5e831e98c7743aee5a4b5c52971c5ce49dda6
navgod <buzz2604@gmail.com>
2024-10-28 16:29:29 +0900

    Title: 구슬의 이동, Time: 190ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241028/구슬의 이동/marble-movement.py
+++ 241028/구슬의 이동/marble-movement.py
@@ -12,14 +12,13 @@ def is_range(x,y):
 def move(bead):
     r,c,d,v = bead
     x,y = c-1,r-1
-    d = dir_map[d]
     dx,dy = dxs[d], dys[d]
     for i in range(v):
         if not is_range(x+dx,y+dy):
             d = (d+2)%4
             dx,dy = dxs[d], dys[d]
         x,y = x+dx, y+dy
-    new_bead = [y+1,x+1,d,v]
+    return [y+1,x+1,d,v]
 
 def check():
     # 속도가 큰순위 같으면 idx가 큰순위
@@ -32,25 +31,32 @@ def check():
         for y in range(n):
             if len(grid[y][x]) >k:
                 target = grid[y][x]
-                selected_beads = sorted(target, key= lambda x: (x[0],x[1]))
+                selected_beads = sorted(target, key= lambda x: (x[0],x[1]), reverse = True)
                 for i in range(k):
                     n_idx = selected_beads[i][1]
                     n_beads.append(beads[n_idx])
             else:
                 if grid[y][x]:
                     for v,i in grid[y][x]:
-                        n_beads.append(beads[i])
+                        if i>= len(beads):
+                            print(i, len(beads), beads)
+                        else:
+                            n_beads.append(beads[i])
+            grid[y][x].clear()
     return n_beads
 
 
 n,m,t,k = map(int, input().split())
-beads = [[int(x[0]), int(x[1]), x[2], int(x[3])] for x in (input().split() for _ in range(m))]
+beads = [[int(x[0]), int(x[1]), dir_map[x[2]], int(x[3])] for x in (input().split() for _ in range(m))]
 grid = [[[] for _ in range(n)] for _ in range(n)]
 
 for _ in range(t):
     new_beads = []
     for bead in beads:
-        new_beads.append(move(bead))
-    bead = new_beads
-    bead = check()
-print(len(bead))
+        new_bead = move(bead)
+        new_beads.append(new_bead)
+        r,c,d,v = new_bead
+    beads = new_beads
+    beads = check()
+
+print(len(beads))
