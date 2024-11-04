0a49a555db322338c22da80b4f8fca8c1b500598
navgod <buzz2604@gmail.com>
2024-11-04 11:47:22 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 142ms, Memory: 19MB, Status: Passed - CodeTree

--- 241104/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 241104/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -1,35 +1,73 @@
+import sys
+
+INT_MAX = sys.maxsize
+MAX_R = 100
+
+# 변수 선언 및 입력:
 n = int(input())
-grid = [list(map(int,input().split())) for _ in range(n)]
+num = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+dp = [
+    [0] * n
+    for _ in range(n)
+]
 
-# 최대 최소
-dp = [[[0,0] for _ in range(n)] for _ in range(n)]
+ans = INT_MAX
 
-dp[0][0][0], dp[0][0][1] = grid[0][0] , grid[0][0]
+def initialize():
+    # 전부 INT_MAX로 초기화합니다.
+    for i in range(n):
+        for j in range(n):
+            dp[i][j] = INT_MAX
 
-for row in range(1,n):
-    dp[row][0][0] = max(dp[row-1][0][0],grid[row][0])
-    dp[row][0][1] = min(dp[row-1][0][1],grid[row][0])
+    # 시작점의 경우 dp[0][0] = num[0][0]으로 초기값을 설정해줍니다
+    dp[0][0] = num[0][0]
 
-for col in range(1,n):
-    dp[0][col][0] = max(dp[0][col-1][0],grid[0][col])
-    dp[0][col][1] = min(dp[0][col-1][1],grid[0][col])
+    # 최좌측 열의 초기값을 설정해줍니다.
+    for i in range(1, n):
+        dp[i][0] = max(dp[i - 1][0], num[i][0])
 
-for row in range(1,n):
-    for col in range(1,n):
-        now = grid[row][col]
-        # 위의 경우
-        up_max = dp[row-1][col][0]
-        up_min = dp[row-1][col][1]
-        ups = [now,up_max,up_min]
-        # 왼쪽의 경우
-        left_max = dp[row][col-1][0]
-        left_min = dp[row][col-1][1]
-        lefts = [now,left_max,left_min]
-
-        if (max(ups)-min(ups)) >= (max(lefts)-min(lefts)):
-            dp[row][col][0],dp[row][col][1]  = max(lefts) , min(lefts)
-        else:
-            dp[row][col][0],dp[row][col][1]  = max(ups) , min(ups)
+    # 최상단 행의 초기값을 설정해줍니다.
+    for j in range(1, n):
+        dp[0][j] = max(dp[0][j - 1], num[0][j])
 
 
-print(dp[n-1][n-1][0]-dp[n-1][n-1][1])
+def solve(lower_bound):
+    # lower_bound 미만의 값은 사용할 수 없도록
+    # num값을 변경해줍니다.
+    for i in range(n):
+        for j in range(n):
+            if num[i][j] < lower_bound:
+                num[i][j] = INT_MAX
+    
+    # DP 초기값 설정
+    initialize()
+
+    # 탐색하는 위치의 위에 값과 좌측 값 중에 작은 값과
+    # 해당 위치의 숫자 중에 최댓값을 구해줍니다.
+    for i in range(1, n):
+        for j in range(1, n):
+            dp[i][j] = max(min(dp[i - 1][j], dp[i][j - 1]), num[i][j])
+        
+    return dp[n - 1][n - 1]
+
+   
+# 최솟값을 k라고 가정했을 때
+# lower_bound 이상의 수들만 사용하여 
+# 이동한다는 조건하에서
+# 최댓값을 최소로 만드는 DP 문제를 풀어줍니다.
+for lower_bound in range(1, MAX_R + 1):
+    upper_bound = solve(lower_bound)
+    
+    # 다 진행했음에도 여전히 INT_MAX라면 
+    # 그러한 이동이 불가능하다는 뜻이므로
+    # 패스합니다.
+    if upper_bound == INT_MAX:
+        continue
+    
+    # 답을 갱신합니다.
+    ans = min(ans, upper_bound - lower_bound)
+
+print(ans)
