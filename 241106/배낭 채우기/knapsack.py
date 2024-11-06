aec93d5aa2f3eb734c3038d2bdaa84f68efa91e1
navgod <buzz2604@gmail.com>
2024-11-06 16:31:44 +0900

    Title: 배낭 채우기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241106/배낭 채우기/knapsack.py
@@ -0,0 +1,17 @@
+n,m = map(int, input().split())
+
+jewel = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [0] * (m+1) # 질량이 m을 담을 수 있는 최대 가치
+dp[0] = 1
+
+def is_range(x):
+    return 0<=x<=m
+
+# initialize
+for w,v in jewel:
+    for i in range(m,-1,-1):
+        if is_range(i-w) and dp[i-w]:
+            dp[i] = dp[i-w] + v 
+
+print(max(dp)-1)
