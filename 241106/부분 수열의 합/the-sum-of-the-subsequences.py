098df51b32de322de226c8fcdf4d9a84151a4f78
navgod <buzz2604@gmail.com>
2024-11-06 16:06:30 +0900

    Title: 부분 수열의 합, Time: 57ms, Memory: 16MB, Status: Passed - CodeTree

--- 241106/부분 수열의 합/the-sum-of-the-subsequences.py
+++ 241106/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -2,17 +2,17 @@ n,m = map(int ,input().split())
 array = list(map(int ,input().split()))
 
 
-dp = [0]*m
+dp = [0]*(m+1)
 dp[0] = 1
 for arr in array:
-    for i in range(m-1,-1,-1):
+    for i in range(m,-1,-1):
         if i-arr <0:
             break
         
         if dp[i-arr]:
             dp[i] = dp[i-arr]
 
-if dp[m-1]:
+if dp[m]:
     print("Yes")
 else:
     print("No")
