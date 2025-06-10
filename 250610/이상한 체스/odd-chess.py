1fed53db70013b10b47e33f4c9c8a4a15b6a2dd8
navgod <buzz2604@gmail.com>
2025-06-10 16:14:10 +0900

    Title: 이상한 체스, Time: 149ms, Memory: 22MB, Status: Passed - Codetree

--- 250610/이상한 체스/odd-chess.py
+++ 250610/이상한 체스/odd-chess.py
@@ -1,53 +1,117 @@
-from copy import deepcopy
+import sys
 
-# 방향: 북, 동, 남, 서 (문제 세팅에 따라 달라질 수 있음)
-dirs = [(-1,0), (0,1), (1,0), (0,-1)] 
+INT_MAX = sys.maxsize
 
-# 각 cctv별 방향 조합 정의 (문제에서 주어진대로 직접 하드코딩)
-cctv_dirs = [
-    [],  # 0번 없음
-    [[0], [1], [2], [3]],                           # 1번
-    [[0,2], [1,3]],                                 # 2번
-    [[0,1], [1,2], [2,3], [3,0]],                   # 3번
-    [[0,1,2], [1,2,3], [2,3,0], [3,0,1]],           # 4번
-    [[0,1,2,3]],                                    # 5번
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+min_area = INT_MAX
+board = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+chess_pieces = [
+    (i, j)
+    for i in range(n)
+    for j in range(m)
+    if 1 <= board[i][j] and board[i][j] <= 5
 ]
 
-n, m = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
+# 말들의 방향을 표시합니다.
+piece_dir = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
 
-cctvs = []
-for i in range(n):
-    for j in range(m):
-        if 1 <= board[i][j] <= 5:
-            cctvs.append((i, j, board[i][j]))
+# 자신의 말로 갈 수 있는 영역을 표시합니다.
+visited = [
+    [False for _ in range(m)]
+    for _ in range(n)
+]
 
-result = n * m
+# 입력으로 주어진 방향에 대해
+# 말의 종류마다 북동남서 방향으로
+# 이동이 가능한지 표시합니다.
+can_move = [
+    [],
+    [1, 0, 0, 0],
+    [0, 1, 0, 1],
+    [1, 1, 0, 0],
+    [1, 1, 0, 1],
+    [1, 1, 1, 1]
+]
 
-def fill(tmp, r, c, directions):
-    for d in directions:
-        nr, nc = r, c
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < m
+
+
+# 움직일 수 있는 곳인지 여부를 확인합니다.
+def can_go(x, y):
+    return in_range(x, y) and board[x][y] != 6
+
+
+# (start_x, start_y)에서 해당 타입의 말이 특정 방향을 
+# 바라보고 있을 때 갈 수 있는 곳들을 전부 표시합니다.
+def fill(start_x, start_y, piece_num, face_dir):
+    # 북동남서 순으로 방향을 설정합니다.
+    dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
+    
+    for i in range(4):
+        # 해당 말이 움직일 수 있는 방향인지를 확인합니다.
+        # 움직일 수 없다면 pass합니다.
+        if not can_move[piece_num][i]:
+            continue
+        
+        # 갈 수 있다면, 끝날때까지 계속 진행합니다.
+        # 방향은 face_dir만큼 시계방향으로 
+        # 회전했을 때를 기준으로 움직입니다.
+        x, y = start_x, start_y
+        move_dir = (i + face_dir) % 4;
         while True:
-            nr += dirs[d][0]
-            nc += dirs[d][1]
-            if not (0 <= nr < n and 0 <= nc < m):
+            visited[x][y] = True
+            nx, ny = x + dxs[move_dir], y + dys[move_dir]
+            if can_go(nx, ny):
+                x, y = nx, ny
+            else:
                 break
-            if tmp[nr][nc] == 6:   # 벽 만나면 중단
-                break
-            if tmp[nr][nc] == 0:   # 감시할 수 있으면 표시
-                tmp[nr][nc] = '#'
 
-def dfs(depth, tmp):
-    global result
-    if depth == len(cctvs):
-        cnt = sum(row.count(0) for row in tmp)
-        result = min(result, cnt)
+
+# n 개의 체스 말의 방향이 정해졌을 때 이동할 수 없는 영역의 넓이를 반환합니다.
+def get_area():
+    # visited 배열을 초기화합니다.
+    for i in range(n):
+        for j in range(m):
+            visited[i][j] = 0
+    
+    for x, y in chess_pieces:
+        # 해당 말이 정해진 방향에 있을 때 갈 수 있는 곳들을 전부 표시합니다.
+        fill(x, y, board[x][y], piece_dir[x][y])
+    
+    area = sum([
+        1
+        for i in range(n)
+        for j in range(m)
+        if visited[i][j] == 0 and board[i][j] != 6
+    ])
+
+    return area
+
+
+def search_min_area(cnt):
+    global min_area
+    
+    # 자신의 말들의 방향이 전부 결정되었을 때, 갈 수 없는 넓이를 계산하여 갱신합니다.
+    if cnt == len(chess_pieces):
+        min_area = min(min_area, get_area())
         return
-    x, y, cctv_type = cctvs[depth]
-    for dirs_case in cctv_dirs[cctv_type]:
-        nxt = deepcopy(tmp)
-        fill(nxt, x, y, dirs_case)
-        dfs(depth+1, nxt)
 
-dfs(0, board)
-print(result)
+    # cnt 말의 방향을 설정합니다. 
+    piece_x, piece_y = chess_pieces[cnt]
+    
+    for i in range(4):
+        piece_dir[piece_x][piece_y] = i
+        search_min_area(cnt + 1)
+
+
+search_min_area(0)
+print(min_area)
